e7400af3fdcf7a26502bc0a135bef6d3175a3e11
Leo Roth <leoroth123@yahoo.com>
2023-06-04 12:49:42 -0700

    Changed Script and Remodeled initial game idea
    -script removes letters as you type the correct letter

---
+++ Assets/typingGeneratedWord.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class typingGeneratedWord : MonoBehaviour
+{
+    public Text shownWord = null;
+
+    private string remainingLetters = string.Empty;
+    private string wordToType = "brother man";
+
+    private void Start()
+    {
+        setWordToType();
+    }
+
+    private void setWordToType()
+    {
+        setRemainingLetters(wordToType);
+    }
+
+    private void setRemainingLetters(string newString)
+    {
+        remainingLetters = newString;
+        shownWord.text = remainingLetters;
+    }
+
+    private void Update()
+    {
+        checkInput();
+    }
+
+    private void checkInput()
+    {
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1) //ensures no error if 2 keys are pressed at the same time
+            {
+                enterLetter(keysPressed);
+            }
+        }
+    }
+
+    private void enterLetter(string typedLetter)
+    {
+        if (isCorrectLetter(typedLetter))
+        {
+            removeLetter();
+
+            if (isWordComplete())
+            {
+                setWordToType();
+            }
+        }
+    }
+
+    private bool isCorrectLetter(string letter)
+    {
+        return remainingLetters.IndexOf(letter) == 0;
+    }
+
+    private void removeLetter()
+    {
+        string newString = remainingLetters.Remove(0, 1);
+        setRemainingLetters(newString);
+    }
+
+    private bool isWordComplete()
+    {
+        return remainingLetters.Length == 0;
+    }
+
+}
