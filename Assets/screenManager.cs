e75009580ca074f5e109aeac828ccdf20c6c5401
Leo Roth <leoroth123@yahoo.com>
2023-06-08 17:45:03 -0700

    Created Screen Manager script and home screen (UI)
    -deleted quit game manager and combined the functionality into screen manager
    -now added a home screen to UI

---
+++ Assets/screenManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenManager : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public GameObject homeScreen;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        homeScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void pauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void resumeGame()
+    {
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
